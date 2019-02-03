275d22793e1c0d101fe582c50ef763329af98b57
Ross Gardler <rgardler@apache.org>
2019-02-03 11:40:39 -0800

    Basic stats system

---
+++ Assets/Digital Painting/Scripts/Statistics/StatModifier.cs
@@ -0,0 +1,29 @@
+﻿namespace wizardscode.statistics
+{
+    public enum StatModifierType
+    {
+        Flat = 100,
+        PercentAdditive = 200,
+        PercentMultiplicative = 300,
+    }
+
+    public class StatModifier
+    {
+
+        public readonly float Value;
+        public readonly StatModifierType Type;
+        public readonly int Order;
+        public readonly object Source;
+
+        public StatModifier(float value, StatModifierType type, object source, int order)
+        {
+            Value = value;
+            Type = type;
+            Order = order;
+            Source = source;
+        }
+
+        public StatModifier(float value, StatModifierType type, object source) : this(value, type, source, (int)type) { }
+
+    }
+}
---
+++ Assets/Digital Painting/Scripts/Statistics/Statistic.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+namespace wizardscode.statistics
+{
+    [Serializable]
+    public class Statistic
+    {
+        public float BaseValue;
+
+        public float Value
+        {
+            get
+            {
+                if (isDirty || BaseValue != lastBaseValue)
+                {
+                    lastBaseValue = BaseValue;
+                    _value = CalculateFinalValue();
+                    isDirty = false;
+                }
+                return _value;
+            }
+        }
+
+        protected bool isDirty = true;
+        protected float _value;
+        protected float lastBaseValue = float.MinValue;
+
+        protected readonly List<StatModifier> statModifiers;
+        public readonly ReadOnlyCollection<StatModifier> StatModifiers;
+
+        public Statistic()
+        {
+            statModifiers = new List<StatModifier>();
+            StatModifiers = statModifiers.AsReadOnly();
+        }
+
+        public Statistic(float baseValue) : this()
+        {
+            BaseValue = baseValue;
+        }
+
+        public void AddModifier(StatModifier mod)
+        {
+            isDirty = true;
+            statModifiers.Add(mod);
+            statModifiers.Sort(CompareModifiersOrder);
+        }
+
+        protected int CompareModifiersOrder(StatModifier a, StatModifier b)
+        {
+            if (a.Order < b.Order)
+            {
+                return -1;
+            }
+            else if (a.Order > b.Order)
+            {
+                return 1;
+            }
+            return 0;
+        }
+
+        public bool RemoveModifier(StatModifier mod)
+        {
+            if (statModifiers.Remove(mod))
+            {
+                isDirty = true;
+                return true;
+            }
+            return false;
+        }
+
+        public bool RemoveAllModifiersFromSource(object source)
+        {
+            bool didRemove = false;
+
+            for (int i = statModifiers.Count - 1; i == 0; i--)
+            {
+                if (statModifiers[i].Source == source)
+                {
+                    isDirty = true;
+                    statModifiers.RemoveAt(i);
+                    didRemove = true;
+                }
+            }
+
+            return didRemove;
+        }
+
+        protected float CalculateFinalValue()
+        {
+            float finalValue = BaseValue;
+            float sumPercentAdd = 0;
+
+            for (int i = 0; i < statModifiers.Count; i++)
+            {
+                StatModifier mod = statModifiers[i];
+
+                switch (mod.Type)
+                {
+                    case StatModifierType.Flat:
+                        finalValue += statModifiers[i].Value;
+                        break;
+                    case StatModifierType.PercentAdditive:
+                        sumPercentAdd += mod.Value;
+
+                        if (i + 1 >= statModifiers.Count || statModifiers[i + 1].Type != StatModifierType.PercentAdditive)
+                        {
+                            finalValue *= sumPercentAdd;
+                        }
+                        break;
+                    case StatModifierType.PercentMultiplicative:
+                        finalValue *= 1 + mod.Value;
+                        break;
+                    default:
+                        Debug.LogWarning("Don't know how to apply StatModifier of type " + mod.Type);
+                        break;
+                }
+            }
+
+            return (float)Math.Round(finalValue, 4);
+        }
+    }
+}
